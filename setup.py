0f42d13505e57f28d384e15bfeede309e3985148
Marko Bastovanovic <markob@vast.com>
2019-03-04 13:39:59 +0100

    Switch to setup.py requirements definition

---
+++ setup.py
@@ -0,0 +1,25 @@
+# coding=utf-8
+import setuptools
+
+setuptools.setup(
+    name='aws-emr-cost-calculator',
+    version='0.0.1',
+    scripts=['aws-emr-cost-calculator'],
+    author="Marko Baštovanović",
+    author_email="marko.bast@gmail.com",
+    description="Utility package to calculate cost of an AWS EMR cluster",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/marko-bast/emr-cost-calculator",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 2.7.9',
+        'Programming Language :: Python :: 3.7',
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=['requests[security]>=2.18.3',
+                      'retrying',
+                      'boto3>=1.9',
+                      'docopt']
+)
